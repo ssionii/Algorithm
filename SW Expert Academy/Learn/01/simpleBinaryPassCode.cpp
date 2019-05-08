c070e8b600e410a6f04cead8b8a82d432acebc39
Yang Si Yeon <mn04098@naver.com>
2019-05-08 17:05:25 +0900

    19/05/08 solved simpleBinaryPassCode - learn 01 problem1

--- SW Expert Academy/Learn/01/simpleBinaryPassCode.cpp
+++ SW Expert Academy/Learn/01/simpleBinaryPassCode.cpp
@@ -1,105 +1,71 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
-int findPassCode(int n, int m, int **arr) {
+
+int getInfo(string passcode) {
+
+	string info[] = { "0001101", "0011001", "0010011", "0111101", "0100011",
+					"0110001", "0101111", "0111011", "0110111", "0001011" };
 	
-	for (int i = 0; i <n ; i++) {
-		for (int j = 0 ; j <m; j++) {
-			cout << arr[i][j] << endl;
-			if (arr[i][j] == 1) {
-				return i * n + j;
-			}
-		}
-	}
-
-	/**
-	for (int i = n - 1; i >= 0; i--) {
-		for (int j = m - 1; j >= 0; j--) {
-			cout << arr[i][j] << endl;
-			if (arr[i][j] == 1) {
-				return i * n + j;
-			}
-		}
-	}
-	*/
-}
-
-int getInfo(int *passcode) {
-
-	int info[] = { 13, 25, 19, 61, 35, 49, 47, 59, 55, 11 };
-	
-	bool flag;
-
 	for (int index = 0; index < 10; index++) {
-		flag = true;
-		for (int i = 6; info[index] > 0; i--) {
-			// 둘이 다르면
-			if ((info[index] % 2) & passcode[i] != passcode[i]) {
-				flag = false;
-				break;
-			}
-			else
-				info[index] = info[index] / 2;
-		}
-		if (flag)
+		if(passcode.compare(info[index]) == 0)
 			return index;
 	}
 }
 
 int main()
 {
-	ifstream in("input.txt");
-	ofstream out("output.txt");
-
 	int test_case;
-	//cin >> test_case;
-	in >> test_case;
+	cin >> test_case;
 
 	for (int case_num = 0; case_num < test_case; case_num++) {
 		int n, m;
-		//cin >> n >> m;
-		in >> n >> m;
+		cin >> n >> m;
 
-
-		// byte여야 함!!!! 난 바보다
-		int **arr = new int*[n];
-
+		string *arr = new string[n];
+		bool flag = false;
 		for (int i = 0; i < n; i++) {
-			arr[i] = new int[m];
-			for (int j = 0; j < m; j++) {
-				in >> arr[i][j];
-				// cin >> arr[i][j];
-			}
-		}
-
-		int passcode[8][7];
-		int index = findPassCode(n, m, arr);
-
-		int vericode[8];
-
-		int y = index / n;
-		int x = index % m;
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 7; j++) {
-				passcode[i][j] = arr[y][(x++) - 55];
-			}
-			vericode[i] = getInfo(passcode[i]);
-		}
-
-		int sum = 0;
-		for (int i = 0; i < 8; i++) {
-			if (i % 2 == 0 && i != 7)
-				sum += vericode[i] * 3;
-			else
-				sum += vericode[i];
-		}
-
-		if (sum % 10 != 0)
-			sum = 0;
+			cin >> arr[i];
 		
-		cout << "#" << case_num << " " << sum << endl;
+			if (arr[i].find("1", 0) != string::npos && flag == false) {
+				flag = true;
+				string temp;
+				for (int j = m - 1; j >= 0; j--) {
+					if (arr[i][j] == '1') {
+						temp = arr[i].substr(j - 55, 56);
+						break;
+					}
+				}
+
+				string passcode[8];
+				int vericode[8];
+				int start = 0;
+				for (int k = 0; k < 8; k++) {
+					passcode[k] = temp.substr(start, 7);
+					vericode[k] = getInfo(passcode[k]);
+					start += 7;
+				}
+
+
+				int sum = 0, result = 0;
+				for (int k = 0; k < 8; k++) {
+					if (k % 2 == 0 && k != 7)
+						sum += vericode[k] * 3;
+					else
+						sum += vericode[k];
+					result += vericode[k];
+				}
+
+				if (sum % 10 != 0)
+					result = 0;
+			
+				cout << "#" << case_num+1 << " " << result << endl;
+			}
+			
+		}
 
 		
 	}
