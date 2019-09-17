dcfef946c641b3182511f6e6693688b416dc3d0d
ssionii <ssionii353@gmail.com>
2019-09-17 22:10:40 +0900

    19/09/17 알고리즘 중

--- SW Expert Academy/Learn/01/scanningPassCode.cpp
+++ SW Expert Academy/Learn/01/scanningPassCode.cpp
@@ -12,6 +12,12 @@ string binaryArr[16] = {
         "1100","1101","1110","1111"
 };
 
+string numArr[10] = {
+        "3211","2221","2122","1411",
+        "1132", "1231", "1114","1312",
+        "1213", "3112"
+};
+
 
 string HexToBinary(string str){
 
@@ -27,6 +33,42 @@ string HexToBinary(string str){
     }
 
     return result;
+}
+
+int getNumFromBinary(string str, int codeLength){
+    bool isZero = true;
+    int tempSize = 0;
+    string result = "";
+
+    for(int i = 0; i < str.size(); i++){
+
+        if(isZero && str[i] == '0'){  // 앞에도 0, 현재도 0
+            tempSize++;
+        }else if(isZero && str[i] == '1'){  // 앞에는 0, 현재는 1
+            result.append(to_string(tempSize));
+            isZero = false;
+            tempSize = 1;
+        }else if(!isZero && str[i] =='0') { // 앞에는 1, 현재는 0
+            result.append(to_string(tempSize));
+            isZero = true;
+            tempSize = 1;
+        }else{ // 앞에도 1, 현재도 1
+            tempSize++;
+        }
+    }
+    result.append(to_string(tempSize));
+
+    for(int i = 0; i< 4; i++){
+        result[i]/=codeLength;
+    }
+
+    int num;
+    for(num = 0; num <10; num++){
+        if(result == numArr[num])
+            break;
+    }
+
+    return num;
 
 }
 
@@ -40,6 +82,8 @@ int main() {
 
         bool notZero = false;
         vector <string> hexCodes;
+
+        int result = 0;
 
         // 입력 받고, 암호 코드를 포함한 행 찾기
         for(int n = 0; n < N; n++){
@@ -69,6 +113,8 @@ int main() {
         // 16진수 배열을 2진수 배열로
         vector <string> binaryCodes;
         vector <string> passCodes;
+        int passCode[8];
+        bool isDuplicate = false;
         for(int i = 0; i<hexCodes.size(); i++){
             binaryCodes.push_back(HexToBinary(hexCodes.at(i)));
 
@@ -79,17 +125,56 @@ int main() {
                     while(binaryCodes.at(i)[l - 56*codeLength] != '0'){
                         codeLength++;
                     }
-                    passCodes.push_back(binaryCodes.at(i).substr(l-(56*codeLength)+1, 56*codeLength));
+
+                    // passCode 중복 없애기
+                    for(int d = 0; d < passCodes.size(); d++){
+                        if(passCodes.at(d) == binaryCodes.at(i).substr(l-(56*codeLength)+1, 56*codeLength)){
+                            isDuplicate = true;
+                            break;
+                        }
+                    }
+                    if(!isDuplicate) {
+                        passCodes.push_back(binaryCodes.at(i).substr(l - (56 * codeLength) + 1, 56 * codeLength));
+                        // passCode 10진수 숫자로 바꾸기?
+
+                        int sum = 0, resultTemp = 0;
+                        for(int index = 0; index < 8; index ++){
+                            passCode[index] = getNumFromBinary(passCodes.back().substr(index*7*codeLength,7*codeLength), codeLength);
+
+                            if(index % 2 == 0) { // 짝수
+                                sum += passCode[index];
+                            }else if((index % 2 == 1) && index != 7){ // 홀수
+                                sum += passCode[index]*3;
+                            }else if(index == 7){
+                                sum += passCode[index];
+                            }
+
+                            cout << passCode[index] <<" ";
+                        }
+
+                        // 검증 코드 확인
+                        if(sum % 10 != 0){
+                            sum -= passCode[7];
+                            int lastCode = 10 - (sum%10);
+                            passCode[7] = lastCode;
+                        }
+
+                        // 결과 출력
+                        for(int index = 0; index < 8; index ++){
+                            cout << passCode[index] <<" ";
+                            resultTemp +=passCode[index];
+                            result = resultTemp;
+                        }
+
+                    }
 
                     l -= 56;
+                    codeLength = 1;
+                    isDuplicate = false;
                 }
             }
         }
 
-
-
-        for(int i = 0; i < passCodes.size(); i++){
-            cout << passCodes.at(i) << endl;
-        }
+        cout << "#" << test_num << " " << result<<endl;
     }
 }
